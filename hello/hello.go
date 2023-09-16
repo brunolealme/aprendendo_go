e2bfc207d04398b2786fb8720ad75a22dd9adfc9
brunolealme <brunolealme@gmail.com>
2023-09-16 00:44:40 -0300

    importando pacote de repositorio externo

--- hello/hello.go
+++ hello/hello.go
@@ -3,8 +3,14 @@ package main
 import (
 	"example/user/hello/morestrings"
 	"fmt"
+
+	"github.com/google/go-cmp/cmp"
 )
 
 func main() {
+	var result string
+	result = morestrings.ReverseRunes("Bruno")
+
 	fmt.Println(morestrings.ReverseRunes("Bruno"))
+	fmt.Println(cmp.Diff(result, result))
 }
